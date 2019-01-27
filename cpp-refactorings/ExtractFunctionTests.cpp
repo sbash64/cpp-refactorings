853845c15bc35bb45adc73361b6346889853e338
sbash64 <sbash64@gmail.com>
2019-01-26 21:40:23 -0600

    refactor

--- cpp-refactorings/ExtractFunctionTests.cpp
+++ cpp-refactorings/ExtractFunctionTests.cpp
@@ -1,18 +1,31 @@
 #include <string>
 #include <vector>
 
+std::vector<std::string::size_type> findBreakPoints(
+	std::string content, 
+	std::vector<int> boundaries
+) {
+	std::vector<std::string::size_type> breakPoints{};
+	auto found = content.find('\n');
+	breakPoints.push_back(found);
+	found = content.find('\n', found + 1);
+	breakPoints.push_back(found);
+	return breakPoints;
+}
+
 std::string extractFunction(
 	std::string original, 
 	std::vector<int> lineBoundaries, 
 	std::string newName
 ) {
+	auto points = findBreakPoints(original, lineBoundaries);
 	return 
-		"void f() {\n"
-		"    b();\n"
-		"}\n"
+		original.substr(0, points.front()) +
+		original.substr(points.back()) +
 		"\n"
-		"void g() {\n"
-		"    a();\n"
+		"\n"
+		"void " + newName + "() {\n" +
+		original.substr(points.front() + 1, points.back() - points.front()) +
 		"}";
 }
 
